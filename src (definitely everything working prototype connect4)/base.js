6a347243da1311310b85532c6beccc760feffed6
Simon <simon@learntalk.org>
2019-05-28 12:25:01 +0800

    Removed unnecessary files and duplicates, updated other files

--- src (definitely everything working prototype connect4)/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyB1lO8zJcK_1Hcb5fVLtGSWa5AwUEzfnK4",
-  authDomain: "react-test-assets.firebaseapp.com",
-  databaseURL: "https://react-test-assets.firebaseio.com",
-});
-
-export default base;
